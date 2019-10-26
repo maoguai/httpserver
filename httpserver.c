4358a63bccc584704c59430b14064cd1c9508dac
丁子恒 <maoguai@dingzihengdeMacBook-Pro.local>
2019-10-26 20:25:20 +0800

    Update httpserver.c

--- httpserver.c
+++ httpserver.c
@@ -11,9 +11,75 @@
 #include <netinet/in.h>
 #include <errno.h>
 
-/*-------------------------全局变量-------------------------*/
+/*-------------------------全局变量--------------------------*/
 #define PORT 6666
+#define BUFFER_SIZE 4096
+#define MAX_QUE_CONN_NM 5
+#define FILE_NAME_MAX 512
+#define SOCKADDR sockaddr_in
+#define S_FAMILY sin_family
+#define SERVER_AF AF_INET
+fd_set block_read_fdset;
+int max_fd;
+#define BOA_FD_SET(fd, where) { FD_SET(fd, where); \
+    if (fd > max_fd) max_fd = fd; \
+    }
+/*-------------------------函数申明--------------------------*/
+void select_loop(int server_s);               /*处理客户端请求*/
+int process_requests(int server_s);           /*报文解析*/
 
+/*----------------------处理客户端请求------------------------*/
+
+void select_loop(int server_s)
+{
+    FD_ZERO(&block_read_fdset);
+    max_fd = server_s+1;
+    while (1) {
+        BOA_FD_SET(server_s, &block_read_fdset); 
+        //没有可读的文件描述符，就阻塞。
+        if (select(max_fd + 1, &block_read_fdset,NULL, NULL,NULL) == -1) {
+            if (errno == EINTR)
+                continue;   /* while(1) */
+            else if (errno != EBADF) {
+                perror("select");
+            }
+        }
+        if (FD_ISSET(server_s, &block_read_fdset))
+            process_requests(server_s);
+    }
+}
+
+/*--------------------------报文解析-------------------------*/
+
+int process_requests(int server_s)
+{
+    int fd;                     /* socket */
+    struct SOCKADDR remote_addr; /* address */
+    int remote_addrlen = sizeof (struct SOCKADDR);
+    size_t len;
+    char buff[BUFFER_SIZE];
+    bzero(buff,BUFFER_SIZE);
+    //remote_addr.S_FAMILY = 0xdead;
+    fd = accept(server_s, (struct sockaddr *) &remote_addr,
+                &remote_addrlen);
+
+    if (fd == -1) {
+        if (errno != EAGAIN && errno != EWOULDBLOCK)
+            /* abnormal error */
+            perror("accept");
+        return -1;
+    }
+
+    int bytes = read(fd, buff, BUFFER_SIZE);
+    if (bytes < 0) {
+        if (errno == EINTR)
+            bytes = 0;
+        else
+            return -1;
+    }
+    printf("recv from client:%s\n",buff);
+    return 0;
+}
 /*--------------------------主函数--------------------------*/
 /*-------------------socket套接字创建TCP连接-----------------*/
 int main(int argc,char* argv[])
@@ -54,5 +120,6 @@ int main(int argc,char* argv[])
         exit(1);
     }
     printf("Listening....\n");
+    select_loop(sockfd);
     return 0;
 }
